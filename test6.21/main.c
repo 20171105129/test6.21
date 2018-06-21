96c7264010ef9d6478f763eb73db2f1ca83ee3c6
yinzichao <s20171105129@admindeimac-33.local>
2018-06-21 09:17:21 +0800

    Initial Commit

---
+++ test6.21/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  test6.21
+//
+//  Created by yinzichao on 18/6/21.
+//  Copyright © 2018年 yinzichao. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
